19cb428aacea731d007cc6fdb13afe2b6b842261
Aresn <admin@aresn.com>
2019-03-12 11:46:23 +0800

    init

---
+++ examples/routers/button.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+    	<Divider>文字按钮</Divider>
+        <Button type="text">文字按钮</Button>
+        <Button type="text" disabled>文字按钮</Button>
+        <Button type="text" class="td-btn-text-primary">文字按钮</Button>
+        <Button type="text" class="td-btn-text-primary" disabled>文字按钮</Button>
+    </div>
+</template>
+<script>
+    export default {
+
+    }
+</script>
---
+++ src/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    filename: 'iview-demo',
+    componentName: 'i-demo',
+    library: 'iViewDemo'
+};
