eca98095b19a4b8b01bd709773beee94aa802c0b
梁灏 <admin@aresn.com>
2019-06-19 12:11:28 +0800

    update Modal

--- examples/routers/alert.vue
+++ examples/routers/alert.vue
@@ -1,25 +1,41 @@
 <template>
     <div>
     	<Divider>基础警告提示</Divider>
-        <Alert closable>消息提示文案</Alert>
-        <Alert closable type="success">成功提示文案</Alert>
-        <Alert closable type="warning">警告提示文案</Alert>
-        <Alert closable type="error">错误提示文案</Alert>
+        <Alert closable>
+            <Icon slot="close" custom="i-td i-td-clear_px" />
+            消息提示文案
+        </Alert>
+        <Alert closable type="success">
+            <Icon slot="close" custom="i-td i-td-clear_px" />
+            成功提示文案
+        </Alert>
+        <Alert closable type="warning">
+            <Icon slot="close" custom="i-td i-td-clear_px" />
+            警告提示文案
+        </Alert>
+        <Alert closable type="error">
+            <Icon slot="close" custom="i-td i-td-clear_px" />
+            错误提示文案
+        </Alert>
         <Divider>带图标的警告提示</Divider>
         <Alert show-icon closable>
             <Icon slot="icon" custom="i-td i-td-info_px" />
+            <Icon slot="close" custom="i-td i-td-clear_px" />
             消息提示文案
         </Alert>
         <Alert show-icon closable type="success">
             <Icon slot="icon" custom="i-td i-td-check_circle_px" />
+            <Icon slot="close" custom="i-td i-td-clear_px" />
             成功提示文案
         </Alert>
         <Alert show-icon closable type="warning">
             <Icon slot="icon" custom="i-td i-td-warning_px" />
+            <Icon slot="close" custom="i-td i-td-clear_px" />
             警告提示文案
         </Alert>
         <Alert show-icon closable type="error">
             <Icon slot="icon" custom="i-td i-td-cancel_px" />
+            <Icon slot="close" custom="i-td i-td-clear_px" />
             错误提示文案
         </Alert>
         <Divider>有辅助文字的警告提示</Divider>
