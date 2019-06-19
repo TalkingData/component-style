7ecf2b49c04d9de21d1432959d35a0928ceeb706
梁灏 <admin@aresn.com>
2019-06-19 13:17:40 +0800

    update Checkbox

--- examples/routers/checkbox.vue
+++ examples/routers/checkbox.vue
@@ -1,18 +1,30 @@
 <template>
     <div>
+        <Checkbox v-model="single">Checkbox</Checkbox>
         <Divider>基础单选框</Divider>
         <CheckboxGroup v-model="animal" size="large">
             <Checkbox label="金斑蝶"></Checkbox>
             <Checkbox label="爪哇犀牛"></Checkbox>
             <Checkbox label="印度黑羚"></Checkbox>
         </CheckboxGroup>
+        <div>
+            <Checkbox v-model="disabledSingle" disabled>Checkbox</Checkbox>
+            <CheckboxGroup v-model="disabledGroup" size="large">
+                <Checkbox label="香蕉" disabled></Checkbox>
+                <Checkbox label="苹果" disabled></Checkbox>
+                <Checkbox label="西瓜"></Checkbox>
+            </CheckboxGroup>
+        </div>
     </div>
 </template>
 <script>
     export default {
         data () {
             return {
-                animal: ['金斑蝶']
+                single: false,
+                animal: ['金斑蝶'],
+                disabledSingle: true,
+                disabledGroup: ['苹果']
             }
         }
     }
