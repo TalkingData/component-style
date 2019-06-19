d8a1333de63199fc85fb734a23274063ccaed25b
梁灏 <admin@aresn.com>
2019-06-19 12:51:35 +0800

    update Radio

--- examples/routers/radio.vue
+++ examples/routers/radio.vue
@@ -12,13 +12,30 @@
             <Radio label="爪哇犀牛"></Radio>
             <Radio label="印度黑羚"></Radio>
         </RadioGroup>
+        <div>
+            <Radio v-model="disabledSingle" disabled>Radio</Radio>
+            <br><br>
+            <RadioGroup v-model="disabledGroup">
+                <Radio label="金斑蝶" disabled></Radio>
+                <Radio label="爪哇犀牛"></Radio>
+                <Radio label="印度黑羚"></Radio>
+            </RadioGroup>
+            <br><br>
+            <RadioGroup v-model="disabledGroup" type="button">
+                <Radio label="金斑蝶" disabled></Radio>
+                <Radio label="爪哇犀牛"></Radio>
+                <Radio label="印度黑羚"></Radio>
+            </RadioGroup>
+        </div>
     </div>
 </template>
 <script>
     export default {
         data () {
             return {
-                animal: '爪哇犀牛'
+                animal: '爪哇犀牛',
+                disabledSingle: true,
+                disabledGroup: '爪哇犀牛'
             }
         }
     }
