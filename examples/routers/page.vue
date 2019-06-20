f940f9e6322a14fe6ddfb545da34571747056197
刘鹏飞 <pengfei.liu@tendcloud.com>
2019-06-20 11:08:40 +0800

    update page

--- examples/routers/page.vue
+++ examples/routers/page.vue
@@ -1,8 +1,21 @@
 <template>
     <div>
     	<Divider>分页</Divider>
-        <Page :total="100" />
-        <Page :total="40" size="small" />
+        <Page :total="100" show-total />
+        <br>
+        <Page :total="100" show-sizer />
+        <br>
+        <Page :total="100" show-elevator />
+        <br>
+        <Page :total="100" show-elevator show-sizer/>
+        <Divider>小型分页</Divider>
+    
+        <Page :total="50" size="small" />
+        <br>
+        <Page :total="50" size="small" show-total />
+        <br>
+        <Page :total="50" size="small" show-elevator show-sizer />
+
     </div>
 </template>
 <script>
