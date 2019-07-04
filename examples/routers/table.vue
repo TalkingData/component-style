57dc6827827f2d9ab4d1ae2e602ea1af2a8338c8
梁灏 <admin@aresn.com>
2019-07-04 14:36:10 +0800

    update Table

--- examples/routers/table.vue
+++ examples/routers/table.vue
@@ -1,164 +1,58 @@
 <template>
-  <div>
-    <Divider>基础表格</Divider>
-
-    <Table :columns="columns1" :data="data1"></Table>
-
-    <Divider>紧凑型表格</Divider>
-
-    <Table :columns="columns1" :data="data1" size="small"></Table>
-
-    <!-- <Table border :columns="columns7" :data="data6"></Table> -->
-  </div>
+    <Table class="td-table-no-border" :columns="columns5" :data="data5"></Table>
 </template>
 <script>
-export default {
-  data() {
-    return {
-      columns1: [
-        {
-          title: "Name",
-          key: "name"
-        },
-        {
-          title: "Age",
-          key: "age"
-        },
-        {
-          title: "Address",
-          key: "address"
-        }
-      ],
-      data1: [
-        {
-          name: "John Brown",
-          age: 18,
-          address: "New York No. 1 Lake Park",
-          date: "2016-10-03"
-        },
-        {
-          name: "Jim Green",
-          age: 24,
-          address: "London No. 1 Lake Park",
-          date: "2016-10-01"
-        },
-        {
-          name: "Joe Black",
-          age: 30,
-          address: "Sydney No. 1 Lake Park",
-          date: "2016-10-02"
-        },
-        {
-          name: "Jon Snow",
-          age: 26,
-          address: "Ottawa No. 2 Lake Park",
-          date: "2016-10-04"
-        }
-      ],
-      columns7: [
-        {
-          title: "Name",
-          key: "name",
-          render: (h, params) => {
-            return h("div", [
-              h("Icon", {
-                props: {
-                  type: "ios-person"
-                }
-              }),
-              h("strong", params.row.name)
-            ]);
-          }
-        },
-        {
-          title: "Age",
-          key: "age"
-        },
-        {
-          title: "Address",
-          key: "address"
-        },
-        {
-          title: "Action",
-          key: "action",
-          width: 150,
-          align: "center",
-          render: (h, params) => {
-            return h("div", [
-              h(
-                "Button",
-                {
-                  props: {
-                    type: "primary",
-                    size: "small"
-                  },
-                  style: {
-                    marginRight: "5px"
-                  },
-                  on: {
-                    click: () => {
-                      this.show(params.index);
+    export default {
+        data () {
+            return {
+                columns5: [
+                    {
+                        title: 'Date',
+                        key: 'date',
+                        sortable: true,
+                        sortType: 'asc'
+                    },
+                    {
+                        title: 'Name',
+                        key: 'name'
+                    },
+                    {
+                        title: 'Age',
+                        key: 'age',
+                        sortable: true
+                    },
+                    {
+                        title: 'Address',
+                        key: 'address'
                     }
-                  }
-                },
-                "View"
-              ),
-              h(
-                "Button",
-                {
-                  props: {
-                    type: "error",
-                    size: "small"
-                  },
-                  on: {
-                    click: () => {
-                      this.remove(params.index);
+                ],
+                data5: [
+                    {
+                        name: 'John Brown',
+                        age: 18,
+                        address: 'New York No. 1 Lake Park',
+                        date: '2016-10-03'
+                    },
+                    {
+                        name: 'Jim Green',
+                        age: 24,
+                        address: 'London No. 1 Lake Park',
+                        date: '2016-10-01'
+                    },
+                    {
+                        name: 'Joe Black',
+                        age: 30,
+                        address: 'Sydney No. 1 Lake Park',
+                        date: '2016-10-02'
+                    },
+                    {
+                        name: 'Jon Snow',
+                        age: 26,
+                        address: 'Ottawa No. 2 Lake Park',
+                        date: '2016-10-04'
                     }
-                  }
-                },
-                "Delete"
-              )
-            ]);
-          }
+                ]
+            }
         }
-      ],
-      data6: [
-        {
-          name: "John Brown",
-          age: 18,
-          address: "New York No. 1 Lake Park"
-        },
-        {
-          name: "Jim Green",
-          age: 24,
-          address: "London No. 1 Lake Park"
-        },
-        {
-          name: "Joe Black",
-          age: 30,
-          address: "Sydney No. 1 Lake Park"
-        },
-        {
-          name: "Jon Snow",
-          age: 26,
-          address: "Ottawa No. 2 Lake Park"
-        }
-      ],
-     
-    };
-  },
-  methods: {
-    show(index) {
-      this.$Modal.info({
-        title: "User Info",
-        content: `Name：${this.data6[index].name}<br>Age：${
-          this.data6[index].age
-        }<br>Address：${this.data6[index].address}`
-      });
-    },
-    remove(index) {
-      this.data6.splice(index, 1);
     }
-  }
-};
 </script>
